4bc3ab294b00229436d42968957e204dbdbbf3fd
IceN9ne <IceN9ne.code@gmail.com>
2021-03-06 19:28:12 -0500

    fix: move pytest-mock to dev-dependencies

--- src/irc_blowfish/__init__.py
+++ src/irc_blowfish/__init__.py
@@ -1,4 +1,10 @@
 from . import base64
 from .crypt import decrypt_cbc, decrypt_ecb, encrypt_cbc, encrypt_ecb
 
-__all__ = ["base64", "decrypt_cbc", "decrypt_ecb", "encrypt_cbc", "encrypt_ecb"]
+__all__ = [
+    "base64",
+    "decrypt_cbc",
+    "decrypt_ecb",
+    "encrypt_cbc",
+    "encrypt_ecb",
+]
